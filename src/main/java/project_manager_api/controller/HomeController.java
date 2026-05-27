3eac31f00756810909330ac6062c8328e28c5252
Piyush Kumar Dubey <144206835+piyushkumardubey@users.noreply.github.com>
2026-05-28 02:47:29 +0530

    Add HomeController with a home endpoint

---
+++ src/main/java/project_manager_api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package project_manager_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Project Manager API Running Successfully";
+    }
+}
